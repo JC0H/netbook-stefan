a7c8b3ffd54c49758f4c59b7a6549fcb39d3e870
JC0H <35865032+JC0H@users.noreply.github.com>
2019-09-11 21:16:01 +0200

    Feature/add view (#16)
    * -Adding main page
    
    * Adding loginf page and security
    
    * show h2 console
    
    * show rest controller get /stefan/products/brand/Lenovo
    
    * add bootstrap
    
    * add bootstrap #1
    
    * resolved conlicts
    
    * extracted versions in pom to properties section

---
+++ src/main/java/pl/laptopy/polizingowe/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package pl.laptopy.polizingowe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/h2-console/**").disable()
+                .headers().frameOptions().disable()
+                .and().authorizeRequests().antMatchers(HttpMethod.GET, "/stefan/products/brand/**").permitAll()
+                .and().authorizeRequests().antMatchers("/", "/home").permitAll()
+                .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("a")
+                        .password("a")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/pl/laptopy/polizingowe/repository/ProductRepository.java
+++ src/main/java/pl/laptopy/polizingowe/repository/ProductRepository.java
@@ -6,7 +6,8 @@ import pl.laptopy.polizingowe.model.Product;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository extends CrudRepository<Product, Long>
+public interface ProductRepository
+        extends CrudRepository<Product, Long>
 {
     Optional<List<Product>> findAllByBrand(String brand);
 }
