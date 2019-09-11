d4cfd7fd4a11ad61d10318de7110d6ced420125f
andri <andriiandrosiukk@gmail.com>
2019-09-11 06:37:39 +0200

    show rest controller get /stefan/products/brand/Lenovo

--- src/main/java/pl/laptopy/polizingowe/config/WebSecurityConfig.java
+++ src/main/java/pl/laptopy/polizingowe/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package pl.laptopy.polizingowe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,8 +18,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/h2-console/**").disable()
                 .headers().frameOptions().disable()
+                .and().authorizeRequests().antMatchers(HttpMethod.GET, "/stefan/products/brand/**").permitAll()
                 .and().authorizeRequests().antMatchers("/", "/home").permitAll()
                 .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
+
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
