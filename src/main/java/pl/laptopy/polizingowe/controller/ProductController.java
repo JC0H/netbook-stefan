dbd79bb5da77aee0b326706b049553d67f2a6b7e
Ostap Shevchenko <ostap.shevhenko95@gmail.com>
2019-09-11 22:09:18 +0200

    refactor

--- src/main/java/pl/laptopy/polizingowe/controller/ProductController.java
+++ src/main/java/pl/laptopy/polizingowe/controller/ProductController.java
@@ -1,7 +1,10 @@
 package pl.laptopy.polizingowe.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.laptopy.polizingowe.model.Product;
 import pl.laptopy.polizingowe.service.ProductService;
 
--- src/main/java/pl/laptopy/polizingowe/model/Product.java
+++ src/main/java/pl/laptopy/polizingowe/model/Product.java
@@ -1,6 +1,9 @@
 package pl.laptopy.polizingowe.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
