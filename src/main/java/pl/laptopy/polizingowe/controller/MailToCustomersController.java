9a7c698bbc54e60f453bc86286a0fad0570512b2
Ostap Shevchenko <ostap.shevhenko95@gmail.com>
2019-09-16 23:07:21 +0200

    added message entity to customers and controller

--- src/main/java/pl/laptopy/polizingowe/controller/MailToCustomersController.java
+++ src/main/java/pl/laptopy/polizingowe/controller/MailToCustomersController.java
@@ -3,22 +3,19 @@ package pl.laptopy.polizingowe.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.laptopy.polizingowe.model.MailToCustomers;
 import pl.laptopy.polizingowe.service.EmailService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("mail")
-public class NotificationController {
+public class MailToCustomersController {
 
     private final EmailService emailService;
 
     @GetMapping
-    public ResponseEntity sendSimpleMailToCustomers(
-            @RequestParam String subject,
-            @RequestParam String text,
-            @RequestParam String to
-    ) {
-        emailService.sendSimpleMessage(subject, text, to);
+    public ResponseEntity sendSimpleMailToCustomers(@RequestBody MailToCustomers mailToCustomers) {
+        emailService.sendSimpleMessage(mailToCustomers);
         return ResponseEntity.ok("emails were sent");
     }
 
--- src/main/java/pl/laptopy/polizingowe/model/MailToCustomers.java
+++ src/main/java/pl/laptopy/polizingowe/model/MailToCustomers.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -21,8 +18,11 @@ public class MailToCustomers {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> receivers;
+
     private String message;
+
     private String subject;
 
 }
