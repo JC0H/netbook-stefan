7fa6416ef92aed9ef728394cb874c51bafa0b25a
Ostap Shevchenko <ostap.shevchenko95@gmail.com>
2020-03-09 21:02:41 +0100

    Feature/show products (#45)
    * All, one + front + by brand
    
    * Security
    
    * Admin buttons
    
    Co-authored-by: Ostap Shevchenko <ostap.shevhenko95@gmail.com>

--- src/main/java/pl/laptopy/polizingowe/config/WebSecurityConfig.java
+++ src/main/java/pl/laptopy/polizingowe/config/WebSecurityConfig.java
@@ -1,38 +1,46 @@
 package pl.laptopy.polizingowe.config;
 
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-public class WebSecurityConfig  {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//    private PropertiesConfig propertiesConfig;
-//
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().ignoringAntMatchers("/h2-console/**").disable()
-//                .headers().frameOptions().disable()
-//                .and().authorizeRequests().antMatchers("/stefan/**").permitAll()
-//                .and().authorizeRequests().antMatchers("/admin/**").permitAll()
-//                .and().authorizeRequests().antMatchers("/", "/home").permitAll()
-//                .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username(propertiesConfig.getSecurityUsername())
-//                        .password(propertiesConfig.getSecurityPassword())
-//                        .roles(Roles.ADMIN.name())
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PropertiesConfig propertiesConfig;
+    private final String DEFAULT_SUCCESSFUL_URL = "/products";
+    private final String ADMIN_ROLE = "ADMIN";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(propertiesConfig.getSecurityUsername())
+                .password(passwordEncoder().encode(propertiesConfig.getSecurityPassword()))
+                .roles(ADMIN_ROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin*").hasRole(ADMIN_ROLE)
+                .antMatchers("/").permitAll()
+                .and().formLogin().defaultSuccessUrl(DEFAULT_SUCCESSFUL_URL, true)
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl(DEFAULT_SUCCESSFUL_URL);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/pl/laptopy/polizingowe/controller/OrderSummaryController.java
+++ src/main/java/pl/laptopy/polizingowe/controller/OrderSummaryController.java
@@ -1,20 +1,19 @@
-package pl.laptopy.polizingowe.rest;
+package pl.laptopy.polizingowe.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import pl.laptopy.polizingowe.config.PropertiesConfig;
 import pl.laptopy.polizingowe.dto.OrderSummaryDto;
-import pl.laptopy.polizingowe.entity.OrderSummary;
 import pl.laptopy.polizingowe.facade.OrderSummaryFacade;
 
 import javax.validation.Valid;
-import java.util.Arrays;
 import java.util.List;
 
-@RestController
-@RequestMapping(value = "${api.stefan.notebook}" + "/orders")
+@Controller
 @RequiredArgsConstructor
+@RequestMapping(value = "${stefan.notebook}" + "/orders")
 public class OrderSummaryController {
 
     private final OrderSummaryFacade orderSummaryFacade;
