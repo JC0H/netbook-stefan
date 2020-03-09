6869f3e2c58031bd945c5cd7555e6b2e5f47257c
Ostap Shevchenko <ostap.shevhenko95@gmail.com>
2020-03-09 22:33:14 +0100

    Admin buttons

--- src/main/java/pl/laptopy/polizingowe/controller/AdminController.java
+++ src/main/java/pl/laptopy/polizingowe/controller/AdminController.java
@@ -1,24 +1,36 @@
 package pl.laptopy.polizingowe.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.laptopy.polizingowe.dto.ProductDto;
+import pl.laptopy.polizingowe.service.ProductService;
 
-import java.util.Collections;
+import java.util.List;
 
 @Controller
+@RequestMapping("/admin/products")
+@RequiredArgsConstructor
 public class AdminController {
 
-    @GetMapping("/admin")
-    public String adminPage(Model model) {
-        model.addAttribute("products", Collections.emptyList());
-        return "/html/products";
+    private final ProductService productService;
+
+    @GetMapping
+    public String getAllProducts(@RequestParam(value = "brand", required = false) String brand,
+                                 Model model) {
+        List<ProductDto> products = brand == null ? productService.findAllProducts() : productService.findAllByBrand(brand);
+        model.addAttribute("products", products);
+        return "html/products";
     }
 
-    @GetMapping("/admin/asd")
-    public String adminPage2(Model model) {
-        model.addAttribute("products", Collections.emptyList());
-        return "/html/products";
+    @GetMapping("/{productId}")
+    public String getOneProduct(@PathVariable Long productId, Model model) {
+        model.addAttribute("oneProduct", productService.findOneProduct(productId));
+        return "html/oneProduct";
     }
 
 }
