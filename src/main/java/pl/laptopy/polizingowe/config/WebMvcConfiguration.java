b698f0bb2043f8f0eafb66a90091676821e58f96
Ostap Shevchenko <ostap.shevhenko95@gmail.com>
2020-04-02 21:41:44 +0200

    Addded default controller

---
+++ src/main/java/pl/laptopy/polizingowe/config/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package pl.laptopy.polizingowe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/products");
+    }
+}
